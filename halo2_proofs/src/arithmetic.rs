4b8e9f6e1972c93872475f9edef9e9967c877fb9
pw-02 <watters.patrick@outlook.com>
2025-08-21 13:56:41 -0700

    let start_exp = 10;     let end_exp = 16;

--- halo2_proofs/src/arithmetic.rs
+++ halo2_proofs/src/arithmetic.rs
@@ -1059,8 +1059,8 @@ fn test_compare_cpu_gpu_msm() {
     // #[cfg(feature = "icicle_gpu")]
     // use crate::best_multiexp_gpu;
 
-    let start_exp = 8;
-    let end_exp = 15;
+    let start_exp = 10;
+    let end_exp = 16;
     let seed = [0u8; 32];
     let mut rng = ChaChaRng::from_seed(seed);
 
